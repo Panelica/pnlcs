06b37bda6057c843537796fb2548efce77d147c5
Panelica User <panelica5rwp@server1.panelica.com>
2026-04-07 00:15:56 +0000

    feat: complete Turkish (TR) translations - admin.php 992 keys + auth.php 12 keys + 2088 DB keys

---
+++ lang/tr/auth.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    "failed" => "Bu kimlik bilgileri kayitlarimizla eslesmiyor.",
+    "password" => "Girilen sifre hatali.",
+    "throttle" => "Cok fazla giris denemesi. Lutfen :seconds saniye sonra tekrar deneyin.",
+    "disabled" => "Bu hesap devre disi birakilmistir.",
+    "admin_login" => "Yonetim Alani",
+    "username" => "Kullanici Adi",
+    "username_placeholder" => "Kullanici adinizi girin",
+    "password_label" => "Sifre",
+    "remember_me" => "Beni hatirla",
+    "sign_in" => "Giris Yap",
+    "signing_in" => "Giris yapiliyor...",
+    "logout" => "Cikis Yap",
+];
