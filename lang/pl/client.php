ed914a163fe911b308901095267ae74dac494154
Grzegorz <hedon77@users.noreply.github.com>
2026-08-15 23:33:14 +0200

    i18n(pl): keep server's 'Przelew bankowy' for funds.bank_transfer

--- lang/pl/client.php
+++ lang/pl/client.php
@@ -453,7 +453,7 @@ return [
     ],
     'funds.amount_range' => 'Minimum $5.00, maksimum $10 000.00',
     'funds.available_credit_desc' => 'Dostępne na faktury i zakupy.',
-    'funds.bank_transfer' => 'Przelew',
+    'funds.bank_transfer' => 'Przelew bankowy',
     'funds.current_credit' => 'Aktualne saldo',
     'funds.custom_amount' => 'Własna kwota',
     'funds.paypal' => 'PayPal',
